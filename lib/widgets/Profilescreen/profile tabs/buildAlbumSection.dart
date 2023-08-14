3bb1a346ba2e8195e7469850ec375de23025a164
Hanz Anthony Cahilog <4troiax@gmail.com>
2023-08-15 06:35:14 +0800

    Fixed Image Picker for Album

--- lib/widgets/Profilescreen/profile tabs/buildAlbumSection.dart
+++ lib/widgets/Profilescreen/profile tabs/buildAlbumSection.dart
@@ -11,33 +11,31 @@ class AlbumSection extends StatefulWidget {
 
 class _AlbumSectionState extends State<AlbumSection> {
   final picker = ImagePicker();
-  final CollectionReference albumRef =
-      FirebaseFirestore.instance.collection('album');
+  final CollectionReference albumRef = FirebaseFirestore.instance.collection('album');
+  final FirebaseStorage _storage = FirebaseStorage.instanceFor(bucket: 'gs://cebuarena-database.appspot.com');
 
-Future<void> pickImage() async {
-  final pickedFile = await picker.pickImage(source: ImageSource.gallery); // Using pickImage method
-  if (pickedFile != null) {
-    final file = File(pickedFile.path);
-    final ref = FirebaseStorage.instance
-        .ref()
-        .child('albums')
-        .child('${DateTime.now().toIso8601String()}.jpg');
-    await ref.putFile(file);
+  Future<void> pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      final file = File(pickedFile.path);
+      final ref = _storage
+          .ref()
+          .child('files')
+          .child('${DateTime.now().toIso8601String()}.jpg');
+      await ref.putFile(file);
 
-    final url = await ref.getDownloadURL();
+      final url = await ref.getDownloadURL();
 
-    albumRef.add({
-      'url': url,
-      'timestamp': Timestamp.fromDate(DateTime.now()),
-    });
+      albumRef.add({
+        'url': url,
+        'timestamp': Timestamp.fromDate(DateTime.now()),
+      });
+    }
   }
-}
-
 
   Future<void> deleteImage(DocumentSnapshot doc) async {
-    final ref = FirebaseStorage.instance.refFromURL(doc['url']);
+    final ref = _storage.refFromURL(doc['url']);
     await ref.delete();
-
     await doc.reference.delete();
   }
 
