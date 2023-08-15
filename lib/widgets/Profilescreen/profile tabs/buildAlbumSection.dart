7c3571677b2eddb4f4eaf44fd1c9ba8024050a44
Hanz Anthony Cahilog <4troiax@gmail.com>
2023-08-15 10:05:51 +0800

    Adjusted Display for Album

--- lib/widgets/Profilescreen/profile tabs/buildAlbumSection.dart
+++ lib/widgets/Profilescreen/profile tabs/buildAlbumSection.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 class AlbumSection extends StatefulWidget {
@@ -11,14 +11,14 @@ class AlbumSection extends StatefulWidget {
 
 class _AlbumSectionState extends State<AlbumSection> {
   final picker = ImagePicker();
-  final CollectionReference albumRef = FirebaseFirestore.instance.collection('album');
-  final FirebaseStorage _storage = FirebaseStorage.instanceFor(bucket: 'gs://cebuarena-database.appspot.com');
+  final CollectionReference albumRef =
+      FirebaseFirestore.instance.collection('album');
 
   Future<void> pickImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       final file = File(pickedFile.path);
-      final ref = _storage
+      final ref = FirebaseStorage.instance
           .ref()
           .child('files')
           .child('${DateTime.now().toIso8601String()}.jpg');
@@ -34,7 +34,7 @@ class _AlbumSectionState extends State<AlbumSection> {
   }
 
   Future<void> deleteImage(DocumentSnapshot doc) async {
-    final ref = _storage.refFromURL(doc['url']);
+    final ref = FirebaseStorage.instance.refFromURL(doc['url']);
     await ref.delete();
     await doc.reference.delete();
   }
@@ -45,8 +45,9 @@ class _AlbumSectionState extends State<AlbumSection> {
       children: [
         ElevatedButton(
           onPressed: pickImage,
-          child: Text('Upload Image'),
+          child: Text('Upload'),
         ),
+        SizedBox(height: 10), // Spacing below the button
         Expanded(
           child: StreamBuilder<QuerySnapshot>(
             stream: albumRef.snapshots(),
@@ -56,21 +57,29 @@ class _AlbumSectionState extends State<AlbumSection> {
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
                     final doc = snapshot.data!.docs[index];
-                    return ListTile(
-                      leading: Image.network(doc['url']),
-                      title: Text('Image'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => deleteImage(doc),
+                    return Container(
+                      height: 1000, // Adjust the height of the ListTile
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(10),
+                        leading: Image.network(
+                          doc['url'],
+                          height: 1000, // Adjust the height of the preview
+                          width: 1000, // Adjust the width of the preview
+                          fit: BoxFit.cover, // Make sure the image covers the entire space
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => deleteImage(doc),
+                        ),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => Dialog(
+                              child: Image.network(doc['url']),
+                            ),
+                          );
+                        },
                       ),
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) => Dialog(
-                            child: Image.network(doc['url']),
-                          ),
-                        );
-                      },
                     );
                   },
                 );
